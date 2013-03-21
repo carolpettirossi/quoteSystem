1cd136d6308ac3cb7870436ebeaac6697fa7c67c
carolpettirossi <carolpettirossi@gmail.com>
2013-03-20 22:44:43 -0300

    Criação da classe MaoDeObra com construtor. Falta getters e setters.

---
+++ quoteSystem/src/br/unesp/igce/quoteSystem/MaoDeObra.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.unesp.igce.quoteSystem;
+
+/**
+ *
+ * @author Carol Pettirossi
+ */
+public class MaoDeObra {
+    String descricao;
+    Float valor;
+    
+    MaoDeObra(String descricao, Float valor) {
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+}
